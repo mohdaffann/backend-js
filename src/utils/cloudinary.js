cb64151b2b0a66c4d880700fa8ee197be35f6be2
muhammadaffan <muhammadaffanpvt@gmail.com>
2025-02-21 23:59:03 +0530

    tokens(password encryp) , multer and cloudinary

---
+++ src/utils/cloudinary.js
@@ -0,0 +1,27 @@
+import { v2 as cloudinary } from "cloudinary";
+import fs from "fs";
+
+import { v2 as cloudinary } from "cloudinary";
+
+cloudinary.config({
+  cloud_name: process.env.CLOUDINARY_CLOUD_NAME,
+
+  api_key: process.env.CLOUDINARY_API_KEY,
+  api_secret: process.env.CLOUDINARY_API_SECRET_KEY,
+});
+
+const uploadOnCloudinary = async (loaclFilePath) => {
+  try {
+    if (!loaclFilePath) return null;
+    const response = await cloudinary.uploader.upload(loaclFilePath, {
+      resource_type: "auto",
+    });
+    console.log("file uploaded successfully!", response.url);
+    return response;
+  } catch (error) {
+    fs.unlinkSync(loaclFilePath);
+    return null;
+  }
+};
+
+export { uploadOnCloudinary };
