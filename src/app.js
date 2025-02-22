d294fa36ca3684d41d031c34f571f8cb8336d165
muhammadaffan <muhammadaffanpvt@gmail.com>
2025-02-23 00:49:11 +0530

    first postman use

--- src/app.js
+++ src/app.js
@@ -28,6 +28,6 @@ import userRouter from "./routes/user.routes.js";
 
 //routes declaration
 
-app.use("./api/v1/users", userRouter);
+app.use("/api/v1/users", userRouter);
 
 export { app };
--- src/controllers/user.controller.js
+++ src/controllers/user.controller.js
@@ -1,6 +1,6 @@
-import { asyncHandler } from "../utils/asyncHandler";
+import { asyncHandler } from "../utils/asyncHandler.js";
 
-const registerUser = asyncHandler(async (requestAnimationFrame, res) => {
+const registerUser = asyncHandler(async (req, res) => {
     res.status(200).json({
         message: "ok",
     });
